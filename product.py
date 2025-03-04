b0277c93c200cf9e3d7cee36400e084f89623374
elisavetTerz <eterzani.atm@gmail.com>
2025-03-04 11:14:18 +0200

    Step 2: Created product.py

---
+++ product.py
@@ -0,0 +1,13 @@
+class Product:
+    def __init__(self, code, name, price):
+        self.code = code
+        self.name = name
+        self.price = price
+    
+    def __repr__(self):
+        return f"Product(code='{self.code}', name='{self.name}', price={self.price})"
+    
+# Test Data: Defining products
+GR1 = Product('GR1', 'Green Tea', 3.11)
+SR1 = Product('SR1', 'Strawberries', 5.00)
+CF1 = Product('CF1', 'Coffee', 11.23)
